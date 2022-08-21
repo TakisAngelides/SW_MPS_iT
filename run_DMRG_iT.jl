f519f54c8e68817c2dbaf9f35803c89675a9ed1c
TakisAngelides <takis.angelides@gmail.com>
2022-08-21 11:02:54 +0200

    current sign in 1st excited algo

--- run_DMRG_iT.jl
+++ run_DMRG_iT.jl
@@ -18,7 +18,7 @@ function run_SW_DMRG_first_excited_state(sites, params, psi_gs, energy_gs)
 
     P = outer(psi_gs', psi_gs)
     H = get_MPO_from_OpSum(get_SW_OpSum(params), sites)
-    Heff = H-energy_gs.*P
+    Heff = H + energy_gs.*P
     energy, psi = DMRG(Heff, sites, params)
 
     return energy, psi
