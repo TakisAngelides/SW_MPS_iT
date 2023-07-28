f03efd28b41371e1792a21ccc7d54a192a9c6231
Takis Angelides <takis.angelides@gmail.com>
2023-07-28 13:28:35 +0200

    fix pipeline for staggered

--- SW_MPS_iT.jl
+++ SW_MPS_iT.jl
@@ -1,28 +1,29 @@
 include("run_DMRG_iT.jl")
 
-N = parse(Int, ARGS[1]) # Number of physical lattice sites
-x = parse(Float64, ARGS[2]) # 1/(ag)^2
-D = parse(Int64, ARGS[3]) # Bond dimension of MPS to be computed
-mg = parse(Float64, ARGS[4]) # m/g
-l_0 = parse(Float64, ARGS[5]) # l_0 = theta/(2*pi)
-lambda = parse(Float64, ARGS[6]) # Lagrange multiplier to enforce total charge squared to be 0
-acc = parse(Float64, ARGS[7]) # Tolerance for stopping condition of the variational algorithm
-ns = parse(Int64, ARGS[8]) # Maximum number of sweeps of the variational algorithm
-D_p = parse(Int64, ARGS[9]) # Bond dimension of ansatz
-mg_p = parse(Float64, ARGS[10]) # m/g of ansatz
-r = parse(Float64, ARGS[11]) # Wilson parameter
-silent = parse(Bool, ARGS[12]) # Whether to compute total charge, energy, average electric field density per sweep
-initial_noise = parse(Float64, ARGS[13]) # Inital noise that decays with each sweep for the DMRG
-w_1_s_2 = parse(Int64, ARGS[14]) # If this is equal to 1 do Wilson, else do staggered
+w_1_s_2 = parse(Int64, ARGS[1])
+N = parse(Int, ARGS[2]) # Number of physical lattice sites
+x = parse(Float64, ARGS[3]) # 1/(ag)^2
+D = parse(Int64, ARGS[4]) # Bond dimension of MPS to be computed
+mg = parse(Float64, ARGS[5]) # m/g
+l_0 = parse(Float64, ARGS[6]) # l_0 = theta/(2*pi)
+lambda = parse(Float64, ARGS[7]) # Lagrange multiplier to enforce total charge squared to be 0
+acc = parse(Float64, ARGS[8]) # Tolerance for stopping condition of the variational algorithm
+ns = parse(Int64, ARGS[9]) # Maximum number of sweeps of the variational algorithm
+D_p = parse(Int64, ARGS[10]) # Bond dimension of ansatz
+mg_p = parse(Float64, ARGS[11]) # m/g of ansatz
+r = parse(Float64, ARGS[12]) # Wilson parameter
+silent = parse(Bool, ARGS[13]) # Whether to compute total charge, energy, average electric field density per sweep
+initial_noise = parse(Float64, ARGS[14]) # Inital noise that decays with each sweep for the DMRG
 first_excited = parse(Bool, ARGS[15]) # If this is true we will compute the first excited state only and not the ground state
+r = 1.0
 
 if !first_excited
 
-    sweep_observables_file_path = "/lustre/fs24/group/group/tangelides/SW_Sweep_Observables_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_r_$(r)_w1s2_$(w_1_s_2)_fe_$(first_excited).txt"
+    sweep_observables_file_path = "/lustre/fs24/group/group/tangelides/SW_Sweep_Observables_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_w1s2_$(w_1_s_2)_fe_$(first_excited).txt"
 
-    mps_file_path = "/lustre/fs24/group/group/tangelides/SW_MPS_States_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_r_$(r)_w1s2_$(w_1_s_2)_fe_$(first_excited).h5"
+    mps_file_path = "/lustre/fs24/group/group/tangelides/SW_MPS_States_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_w1s2_$(w_1_s_2)_fe_$(first_excited).h5"
 
-    previous_mps_file_path = "/lustre/fs24/group/group/tangelides/SW_MPS_States_iT/N_$(N)_x_$(x)_D_$(D_p)_l0_$(l_0)_mg_$(mg_p)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_r_$(r)_w1s2_$(w_1_s_2)_fe_$(first_excited).h5"
+    previous_mps_file_path = "/lustre/fs24/group/group/tangelides/SW_MPS_States_iT/N_$(N)_x_$(x)_D_$(D_p)_l0_$(l_0)_mg_$(mg_p)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_w1s2_$(w_1_s_2)_fe_$(first_excited).h5"
 
     if isfile(previous_mps_file_path)
         f = h5open(previous_mps_file_path, "r")
@@ -38,13 +39,12 @@ if !first_excited
         previous_psi = randomMPS(sites, D)
     end
 
-    params = Dict("first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mg" => mg, "r" => r, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
-
-    # Compute the MPS
-
     if w_1_s_2 == 1
+        params = Dict("first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mg" => mg, "r" => r, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
         H = get_MPO_from_OpSum(get_SW_OpSum(params), sites)
-    else
+    else 
+        mu = 2*mg*sqrt(x)
+        params = Dict("first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mu" => mu, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
         H = get_MPO_from_OpSum(get_Schwinger_staggered_Hamiltonian_OpSum(params), sites)
     end
 
@@ -70,6 +70,7 @@ else
     mps_file_path = "/lustre/fs24/group/cqta/tangelides/SW_MPS_First_Excited_States_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_r_$(r)_w1s2_$(w_1_s_2)_fe_$(first_excited).h5"
 
     psi_0_file_path = "/lustre/fs24/group/cqta/tangelides/SW_MPS_States_iT/N_$(N)_x_$(x)_D_$(D)_l0_$(l_0)_mg_$(mg)_ns_$(ns)_acc_$(acc)_lam_$(lambda)_r_$(r)_w1s2_$(w_1_s_2)_fe_false.h5"
+    
     f = h5open(psi_0_file_path, "r")
     psi_0 = read(f, "MPS", MPS)
     energy_0 = read(f, "gs_energy")
@@ -84,13 +85,12 @@ else
         previous_psi = randomMPS(sites, D)
     end
 
-    # Compute the MPS
-
-    params = Dict("Ms" => [psi_0], "w" => abs(energy_0), "first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mg" => mg, "r" => r, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
-
     if w_1_s_2 == 1
+        params = Dict("Ms" => [psi_0], "w" => abs(energy_0), "first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mg" => mg, "r" => r, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
         H = get_MPO_from_OpSum(get_SW_OpSum(params), sites)
-    else
+    else 
+        mu = 2*mg*sqrt(x)
+        params = Dict("Ms" => [psi_0], "w" => abs(energy_0), "first_excited" => first_excited, "initial_noise" => initial_noise, "silent" => silent, "N" => N, "D" => D, "x" => x, "ns" => ns, "lambda" => lambda, "l_0" => l_0, "mu" => mu, "acc" => acc, "sweep_observables_file_path" => sweep_observables_file_path, "previous_mps_file_path" => previous_mps_file_path, "previous_psi" => previous_psi)
         H = get_MPO_from_OpSum(get_Schwinger_staggered_Hamiltonian_OpSum(params), sites)
     end
 
